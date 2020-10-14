06b8edb8fee57668865d88158e87e95da5d4b0eb
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-10-13 21:31:30 -0400

    Create orphanages-map.css, orphanagesMap, corrigido rota de OrphanagesMap e add funcão Link pra SPA

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ yarn create react-app happy-web --template typescript
 yarn add react-icons
 yarn add react-router-dom
 yarn add @types/react-router-dom -D
+yarn add leaflet react-leaflet
+yarn add @types/react-leaflet -D
+
 ```
--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
     return(
-        <h1>Pagina OrphanagesMap</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Cuiabá</strong>
+                    <span>Mato Grosso</span>
+                </footer>
+            </aside>
+
+            <Map 
+                center={[-15.6371828,-56.0138516]}
+                zoom={15}
+                style={{ width: '100%', height:'100%' }}
+            >
+            <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            {/**<TileLayer 
+            url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=S{process.env.REACT_APP_MAPBOX_TOKEN}`}  */}
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
+
+        </div>
     )
 }
 export default OrphanagesMap;
