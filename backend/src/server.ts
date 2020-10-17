c985ce6ec0a96e8ad41b9b15fe5a50763a36680f
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-10-16 21:28:38 -0400

    criado images_view.ts para vizualizar as imagens como links url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'path';
+
 import './database/connection';
 
 import routes from './routes';
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 
 // Definindo porta localhost:3333
