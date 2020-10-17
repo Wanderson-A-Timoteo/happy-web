fa9e738a496f6d35f9608b3faec5a87975334506
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-10-17 11:14:10 -0400

    corrigido rota :id, deletado todo o bd e criado novo orfanato, listado e buscado por id, tudo certo!

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -24,7 +24,7 @@ const upload = multer(uploadConfig);
 
 
 routes.get('/orphanages', OrphanagesController.index);
-routes.get('/orphanages/id:', OrphanagesController.show);
+routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 
